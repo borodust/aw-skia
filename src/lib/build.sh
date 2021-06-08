a549e53054fa3ef04bd2a5b30b122647bc7ed4a8
Pavel Korolev <dev@borodust.org>
2021-06-08 21:52:03 +0300

    Use minimal Skia build

--- src/lib/build.sh
+++ src/lib/build.sh
@@ -70,51 +70,44 @@ function build_desktop {
 cd $LIBRARY_DIR
 python2 tools/git-sync-deps
 
-       # skia_use_angle=false \
-       # skia_use_dng_sdk=false \
-       # skia_use_expat=false \
-       # skia_use_fontconfig=false \
-       # skia_use_freetype=true \
-       # skia_use_libheif=false \
-       # skia_use_libjpeg_turbo_decode=true \
-       # skia_use_libjpeg_turbo_encode=false \
-       # skia_use_libpng_decode=true \
-       # skia_use_libpng_encode=false \
-       # skia_use_libwebp_decode=false \
-       # skia_use_libwebp_encode=false \
-       # skia_use_wuffs=false \
-
-       # skia_enable_tools=false \
-       # skia_enable_skshaper=false \
-       # skia_enable_ccpr=false \
-       # skia_enable_nvpr=false \
-       # skia_enable_fontmgr_custom_directory=false \
-       # skia_enable_fontmgr_custom_embedded=false \
-       # skia_enable_fontmgr_custom_empty=false \
-       # skia_enable_pdf=false \
-
-       # skia_use_system_libpng=false \
-       # skia_use_system_freetype2=false \
-       # skia_use_system_libjpeg_turbo=false \
-       # skia_use_system_libwebp=false \
-       # skia_use_system_zlib=false \
-       # skia_use_system_icu=false \
-       # skia_use_system_harfbuzz=false \
-
 bin/gn gen out/Clawed --args="is_official_build=false \
        is_debug=true \
        is_component_build=true \
-
        skia_use_gl=true \
        skia_use_vulkan=false \
-
        skia_use_egl=false \
        skia_use_x11=true \
-
        skia_use_lua=false \
        skia_use_piex=false \
        skia_use_zlib=true \
-
+       skia_use_system_libpng=false \
+       skia_use_system_freetype2=false \
+       skia_use_system_libjpeg_turbo=false \
+       skia_use_system_libwebp=false \
+       skia_use_system_zlib=false \
+       skia_use_system_icu=false \
+       skia_use_system_harfbuzz=false \
+       skia_enable_tools=false \
+       skia_enable_skshaper=false \
+       skia_enable_ccpr=false \
+       skia_enable_nvpr=false \
+       skia_enable_fontmgr_custom_directory=false \
+       skia_enable_fontmgr_custom_embedded=false \
+       skia_enable_fontmgr_custom_empty=false \
+       skia_enable_pdf=false \
+       skia_use_angle=false \
+       skia_use_dng_sdk=false \
+       skia_use_expat=false \
+       skia_use_fontconfig=false \
+       skia_use_freetype=true \
+       skia_use_libheif=false \
+       skia_use_libjpeg_turbo_decode=true \
+       skia_use_libjpeg_turbo_encode=false \
+       skia_use_libpng_decode=true \
+       skia_use_libpng_encode=false \
+       skia_use_libwebp_decode=false \
+       skia_use_libwebp_encode=false \
+       skia_use_wuffs=false \
        cc=\"$CC\" cxx=\"$CXX\""
 ninja -C out/Clawed
 
