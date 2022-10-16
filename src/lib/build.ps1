244b5089c3e2e2e5e19fc80e4d5682fa7a8323d3
Pavel Korolev <dev@borodust.org>
2022-10-16 13:12:59 +0200

    Use ClangCL workflow for win build

--- src/lib/build.ps1
+++ src/lib/build.ps1
@@ -28,9 +28,14 @@ $LibraryDir = "$WorkDir/skia/"
 
 pushd $LibraryDir
 
-$GnArgs=@'
-is_official_build=true
-clang_win=\"C:/Program Files/LLVM\"
+if ( $BuildType -eq "Debug" ) {
+    $GnArgs = "is_official_build=false`n"
+} else {
+    $GnArgs = "is_official_build=true`n"
+}
+
+$GnArgs += @'
+clang_win=\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/Llvm/x64\"
 cc=\"clang\"
 cxx=\"clang++\"
 extra_cflags=[\"-Wno-invalid-offsetof\"]
@@ -40,7 +45,7 @@ skia_use_vulkan=false
 skia_use_egl=false
 skia_use_lua=false
 skia_use_piex=false
-skia_use_zlib=true
+skia_use_zlib=false
 skia_use_system_zlib=false
 skia_use_system_icu=false
 skia_enable_tools=false
@@ -59,8 +64,8 @@ skia_use_system_freetype2=false
 skia_use_libheif=false
 skia_use_libjpeg_turbo_decode=false
 skia_use_libjpeg_turbo_encode=false
-skia_use_libpng_decode=true
-skia_use_libpng_encode=true
+skia_use_libpng_decode=false
+skia_use_libpng_encode=false
 skia_use_system_libpng=false
 skia_use_libwebp_decode=false
 skia_use_libwebp_encode=false
@@ -80,7 +85,7 @@ popd
 md $BuildDir -Force | Out-Null
 pushd $BuildDir
 
-cmake -G "Visual Studio 16 2019" -A x64 -Thost=x64 `
+cmake -G "Visual Studio 16 2019" -A x64 -T ClangCL `
   "$WorkDir"
 
 cmake --build "$BuildDir" --config $BuildType --parallel $BuildThreadCount
